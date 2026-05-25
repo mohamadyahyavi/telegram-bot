8242a4e543741147154628a454b224a71500dd0e
mohamad yahyavi <mohamadyahyavi406@gmail.com>
2026-05-25 11:44:07 +0330

    first telegram bot

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, Request
+
+app = FastAPI()
+
+@app.post("/webhook")
+async def webhook(request: Request):
+
+    data = await request.json()
+
+    print(data)
+
+    return {"ok": True}
